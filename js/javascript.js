cd3f3c5bbc004dc4662aaac0fbd12b7725aac9bd
pep04706 <paula.perez-rubio@ur.de>
2017-08-30 00:59:50 +0200

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,59 @@
+// Hover over Links
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function(){
+    $('[data-toggle="dropdown"]').tooltip();
+});
+
+
+
+
+var furniture_imgs = ['beach_chair.jpg', 'bed.jpg', 'bookshelf.jpg', 
+    'chair_1.jpg',  'chair_2.jpg', 'chair_3.jpg', 
+    'little_table.jpg', 'sofa_bed.jpg', 'storage_furniture.jpg']
+
+var household_imgs = ['clotheshorse.jpg',  'dehumificator.jpg',  'duvet.jpg',  
+    'paperbin.jpg',  'pillow.jpg',  'shoerack1.jpg',  'shoerack2.jpg']
+
+
+var games_imgs = ['anno_domini.jpg', 'carcassone.jpg', 'dominion_intrigue.jpg', 
+'puzzle_escher2.jpg', 'puzzle_marocco.jpg', 'puzzle_rizzi.jpg', 'suburbia.jpg',
+'back_gammon.jpg', 'dominion_bluetezeit.jpg', 'dominion_thealchimist.jpg',  
+'puzzle_gray.jpg', 'puzzle_neuschwanstein.jpg', 'settlers_of_catan.jpg',
+'black_stories.jpg', 'dominion_darkages.jpg', 'puzzle_escher1.jpg',
+'puzzle_jovendelaperla.jpg', 'puzzle_paris.jpg', 'star_realms.jpg']
+
+
+var sport_imgs = ['basketball.jpg', 'bike1.jpg',  'bike2.jpg',  
+    'bike_bjorn.jpg',  'football.jpg']
+
+
+var garden_imgs = ['jardineras.jpg', "maceta.jpg"]
+
+var cables_imgs = ['nopics.png', 'umbrella.jpg']
+
+/**
+ *  * Returns a random integer between min (inclusive) and max (inclusive)
+ *   * Using Math.round() will give you a non-uniform distribution!
+ *    */
+function getRandomInt(min, max) {
+       return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function displayNextImage(id, imgs, path) {
+//    x = (x === imgs.length - 1) ? 0 : x + 1;
+    x = getRandomInt(0,imgs.length-1)
+    document.getElementById(id).src =  path + imgs[x];
+}
+
+
+function startTimer(id, imgs, path,t) {
+    setInterval(function (){ displayNextImage(id, imgs, path)}, t);
+
+}
+
+
+
+
